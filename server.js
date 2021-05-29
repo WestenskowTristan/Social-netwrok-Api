7b725f27e06a02352851c1e7f2188f5d66037494
tristan <westenskowtristan122@gmail.com>
2021-05-29 11:59:58 -0600

    added user api and controllers

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.use(require("./routes"));
 
 mongoose.connect(
@@ -18,7 +19,6 @@ mongoose.connect(
   }
 );
 
-// Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
